564b02d0a2fbe7bdd047fb712afa20b75d7ebb3c
manbodhvikram <manbodhvikram@gmail.com>
2023-08-21 15:24:45 +0530

    submitting code-Mon Aug 21 2023 15:24:41 GMT+0530 (India Standard Time)

---
+++ src/components/Child.js
@@ -0,0 +1,31 @@
+// import React,{useState} from "react";
+
+
+
+// const Child=({isLoggedIn,setIsLoggedIn})=>{
+//     let[Username,setUserName]=useState("")
+//     let[password,setPassword]=useState("")
+
+//     const logIn=()=>{
+//         if(!Username||!password){
+
+//         setIsLoggedIn("you are logged in")
+//         setIsLoggedIn(true);
+//     }
+
+//     }
+
+
+//     return(
+
+
+//         <div>
+
+//                    Username: <input type="text" onChange={(e)=>setUserName(e.target.value)}></input><br /><br />
+
+//                    Password: <input type="password" onChange={(e)=>setPassword(e.target.value)}></input><br /><br />
+//                   <button onClick={logIn}>Login</button>
+//         </div>
+//     )
+// }
+// export default Child;
---
+++ src/components/Parent.js
@@ -0,0 +1,26 @@
+// import React,{useState} from "react";
+// import Child from "./Child";
+
+
+
+
+// const Parent=()=>{
+//     let[isLoggedIn,setIsLoggedIn]=useState(false)
+   
+
+
+
+  
+
+
+//     return(
+//         <div>
+//             <h1>Parent Component</h1>
+//             <h3>{setIsLoggedIn}</h3>
+
+         
+//             <Child isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn}></Child>
+//         </div>
+//     )
+// }
+// export default Parent;
